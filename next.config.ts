63b906b13cf83e500ba5e467bacf99999cc00875
SyamilEftech <mr.meorsyamil@gmail.com>
2026-05-11 07:07:23 +0800

    Revert "BIG: Push Notifications"
    This reverts commit 39cf43fdca6df1d9966ccaec0e9ab770710a716b.

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,7 @@
 import type { NextConfig } from "next";
-import createPWAConfig from "@ducanh2912/next-pwa";
-
-const pwaConfig = createPWAConfig({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: false,
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/meorfitnesspal\.vercel\.app\/.*/i,
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "offline-runtime",
-        expiration: {
-          maxEntries: 10,
-          maxAgeSeconds: 60 * 60 * 24 * 365,
-        },
-        networkTimeoutSeconds: 10,
-      },
-    },
-  ],
-});
 
 const nextConfig: NextConfig = {
-  ...pwaConfig,
+  /* config options here */
   allowedDevOrigins: ["914e-180-74-71-109.ngrok-free.app"],
 };
 
